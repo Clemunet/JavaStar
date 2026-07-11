5b031dc0fb2963e6635b6e0318f705be29f64a10
clemunet <clement.charbonnel@gmail.com>
2026-07-11 16:35:23 +0200

    Implémentation des ennemis, projectiles et collisions

--- JS/main.js
+++ JS/main.js
@@ -2,19 +2,26 @@ const canvas = document.querySelector('#gameCanvas'); //recup le canvas dans le 
 
 const ctx = canvas.getContext("2d"); // recup le context
 
-canvas.width = 1000;
-canvas.height = 700;
+canvas.width = (2000);
+canvas.height = (1200);
 
 
 // Vaisseau LeMich et ClemShip--------------------------------------------------
 
 const leMichShip = {
 
-    x: 300, y: 600, width: 60, height: 60, speed: 5, lives: 3, score: 0
+    x: 300, y: 600, width: 60, height: 60, speed: 5, fireCooldown: 10, lives: 3, score: 0
 }
+
+let bullets = [];
+let enemies = [];
+let enemySpawnCooldown = 30;
+
+
+
 const ClemShip = {
 
-    x: 600, y: 600, width: 60, height: 60,speed: 5, lives: 3, score: 0
+    x: 600, y: 600, width: 60, height: 60,speed: 5, fireCooldown: 10, lives: 3, score: 0
 };
 
 //clavier----------------------------
@@ -47,7 +54,7 @@ function drawBackground() {
 /*ctx.fillStyle = "black"; //couleur de remplissage
 ctx.fillRect(0, 0, canvas.width, canvas.height); //rect noir pour remplissage canvas*/
 
-// draw vaisseau---------------------------------------------------------------------
+// draw vaisseaux---------------------------------------------------------------------
 
 function drawShip(ship)  {
     ctx.fillStyle = "violet";
@@ -59,61 +66,220 @@ function drawShip(ship)  {
     ctx.fill();
 }
 
+function drawBullet(bullet) {
+    ctx.fillStyle = "green";
+
+        ctx.fillRect(bullet.x, bullet.y, bullet.width, bullet.height);
+
+}
+function drawBullets() {
+    for (const bullet of bullets) {
+        drawBullet(bullet);
+    }
+}
+    //draw enemy
+    function drawEnemy(enemy) {
+
+        ctx.fillStyle = "grey";
+
+        ctx.fillRect(enemy.x, enemy.y, enemy.width, enemy.height)
+    }
+    function drawEnemies() {
+        for (const enemy of enemies) {
+            drawEnemy(enemy);
+        }
+
+
+}
+
+function shoot(ship) {
+    const bullet = {
+
+        x: ship.x + ship.width / 2 - 3,
+        y: ship.y,
+
+        width: 6,
+        height: 15,
+
+        speed: 10,
+
+        alive: true
+    };
+
+    bullets.push(bullet);
+}
+// Enemies-------------------------------------------------------
+function spawnEnemy() {
+
+    const enemy = {
+        x: Math.floor(Math.random() * (canvas.width - 50)),
+        y: -50, width: 50, height: 50, speed: 2,
+        alive: true
+    };
+    enemies.push(enemy);
+
+}
+
 function updatePlayer() {
 
 
- //leMichShip
+    //cooldown lemich
+    if (leMichShip.fireCooldown > 0) {
+        leMichShip.fireCooldown--;
+    }
+
+
+    //leMichShip controls
     if (keys["ArrowLeft"]) {
 
-        leMichShip.x -= 5;
+        leMichShip.x -= leMichShip.speed;
     }
     if (keys["ArrowRight"]) {
 
-        leMichShip.x += 5;
+        leMichShip.x += leMichShip.speed;
     }
     if (keys["ArrowUp"]) {
 
-        leMichShip.y -= 5;
+        leMichShip.y -= leMichShip.speed;
     }
     if (keys["ArrowDown"]) {
 
-        leMichShip.y += 5;
+        leMichShip.y += leMichShip.speed;
     }
-// clemShip
+
+    if (keys["Space"] && leMichShip.fireCooldown === 0) {
+
+        shoot(leMichShip);
+        leMichShip.fireCooldown = 10;
+    }
+
+//cooldown clemship
+
+    if (ClemShip.fireCooldown > 0) {
+        ClemShip.fireCooldown--;
+    }
+
+
+// clemShip controls
 
     if (keys["q"]) {
 
-        ClemShip.x -= 5;
+        ClemShip.x -= ClemShip.speed;
     }
     if (keys["d"]) {
 
-        ClemShip.x += 5;
+        ClemShip.x += ClemShip.speed;
     }
     if (keys["z"]) {
 
-        ClemShip.y -= 5;
+        ClemShip.y -= ClemShip.speed;
     }
     if (keys["s"]) {
 
-        ClemShip.y += 5;
+        ClemShip.y += ClemShip.speed;
+    }
+    if (keys["CapsLock"] && ClemShip.fireCooldown=== 0) {
+
+        shoot(ClemShip);
+        ClemShip.fireCooldown = 10;
+    }
+}
+
+
+    function updateBullets() {
+        for (const bullet of bullets) {
+            bullet.y -= bullet.speed;
+        }
+        //bullets = bullets.filter(function (bullet) {
+           // return bullet.alive && bullet.y + bullet.height > 0;
+        //});
+    }
+        function updateEnemies() {
+            for (const enemy of enemies) {
+                enemy.y += enemy.speed;
+            }
+            //enemies = enemies.filter(function (enemy) {
+               // return enemy.alive && enemy.y < canvas.height;
+            //});
+        }
+
+function cleanObjects() {
+
+    bullets = bullets.filter(function (bullet) {
+
+        return bullet.alive && bullet.y + bullet.height > 0;
+
+    });
+
+    enemies = enemies.filter(function (enemy) {
+
+        return enemy.alive && enemy.y < canvas.height;
+
+    });
+
+}
+
+
+
+function checkCollision(bullet, enemy) {
+
+    if (
+        bullet.x + bullet.width < enemy.x ||
+        bullet.x > enemy.x + enemy.width ||
+        bullet.y + bullet.height < enemy.y ||
+        bullet.y > enemy.y + enemy.height
+    ) {
+        return false;
+    }
+    return true;
+}
+function checkBulletHits() {
+
+    for (const bullet of bullets) {
+        for (const enemy of enemies) {
+            if (checkCollision(bullet, enemy)) {
+
+                bullet.alive = false;
+                enemy.alive = false;
+
+            }
+        }
+    }
+}
+    function update() {
+
+        updatePlayer();
+        updateBullets()
+        updateEnemies()
+        checkBulletHits();
+        cleanObjects();
+
+
+        enemySpawnCooldown--;
+        if (enemySpawnCooldown <= 0) {
+            spawnEnemy();
+            enemySpawnCooldown = 30;
+        }
     }
 
-}
-
-function update() {
-
-    updatePlayer();
-}
-
 // boucle du jeu--------------------------------------------------------------------------
-function gameLoop() {
+    function gameLoop() {
 
-    update();
-    drawBackground();
+        update();
 
-    drawShip(leMichShip);
-    drawShip(ClemShip);
+        // shoot(leMichShip); //test
 
-    requestAnimationFrame(gameLoop);
-}
+        drawBackground();
+
+        drawBullets();
+
+        drawEnemies()
+
+        drawShip(leMichShip);
+        drawShip(ClemShip);
+
+
+        requestAnimationFrame(gameLoop);
+    }
+
 gameLoop();
